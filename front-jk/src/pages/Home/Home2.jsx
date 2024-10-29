ede87bf1bcc365ce819b7b4593719ebc78479bff
marcosviniluz <marcos.almeida@sptech.school>
2024-10-29 20:37:28 -0300

    ALteração nas telas de landing page e home logada, faltando as imagens

--- front-jk/src/pages/Home/Home2.jsx
+++ front-jk/src/pages/Home/Home2.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
-import logo from './img/logo2.png';
-import fotoPerfil from './img/perfil.jpg';
-import HeaderLogado from './components/HeaderLogado/HeaderLogado';
+import styles from '../Home/Home2.module.css';
+import logo from '../../img/logo2.png';
+import fotoPerfil from '../../img/perfil.jpg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import HeaderLogado from '../../components/HeaderLogado/HeaderLogado';
+import { FaChevronLeft } from 'react-icons/fa';
+import { FaChevronRight } from 'react-icons/fa';
 
-// Styled components para layout
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   font-family: Arial, sans-serif;
   background-color: #F0F0F0;
 `;
-
-const Aativo = styled.a`
- color:black  ;
-`;
-
 
 const Header = styled.header`
   background-color: #2B4C7E;
@@ -74,16 +74,12 @@ const PrayerSection = styled.div`
   border-radius: 10px;
   width: 550px;
   height: 50%;
-
-    margin-bottom: 20px;
+  margin-bottom: 20px;
   margin-right: 20px;
 
   h2 {
-    margin-bottom: 10px;
-    font-size: 1.5rem;
-    text-align: ;
     margin-bottom: 25px;
-
+    font-size: 1.5rem;
   }
 
   label {
@@ -99,7 +95,6 @@ const PrayerSection = styled.div`
     margin-bottom: 30px;
     border: 1px solid #ccc;
     border-radius: 5px;
-
   }
 
   textarea {
@@ -108,7 +103,6 @@ const PrayerSection = styled.div`
   }
 
   button {
-    margin-width: -10px;
     width: 100%;
     padding: 10px 0;
     border: none;
@@ -128,15 +122,21 @@ const PrayerSection = styled.div`
 const CarouselContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-itens: center;
+  height:100px;
   gap: 20px;
   width: 100%;
 `;
 
 const Section = styled.div`
   background-color: #D9D9D9;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   border-radius: 10px;
   padding: 10px;
   width: 100%;
+  align-items: center;
 
   h2 {
     margin-bottom: 10px;
@@ -147,9 +147,26 @@ const Section = styled.div`
 
 const Carousel = styled.div`
   display: flex;
-  align-items: center;
-  position: relative;
+  height: 30vh;
+  align-tiens: center;
+  justify-content: space-between; 
+  width:60vw; 
+
+  img{
+   border-radius: 10px;
+    width:100%;
+    height: 30vh;
+  }
+
+  button{
+  background: none;
+    border: none;
+    font-size: 3rem;
+    cursor: pointer;
+    color: #000;
+  }
 `;
+
 
 const ArrowButton = styled.button`
   background: none;
@@ -176,92 +193,108 @@ const ImageContainer = styled.div`
 `;
 
 const Home2 = () => {
-  const images = [
-    'https://jpimg.com.br/uploads/2023/01/design-sem-nome-2023-01-18t104305.427-750x432.png',
-    'https://www.tnh1.com.br//fileadmin/_processed_/c/7/csm_deolane_presa_-_foto_montagem_tnh1_3ee9d26e74.jpg',
+  const swiperRef = useRef(null);
+  const sections = [
+    {
+      images: [
+        { url: 'https://jpimg.com.br/uploads/2023/01/design-sem-nome-2023-01-18t104305.427-750x432.png' },
+        { url: 'https://www.tnh1.com.br//fileadmin/_processed_/c/7/csm_deolane_presa_-_foto_montagem_tnh1_3ee9d26e74.jpg' }
+      ]
+    }
   ];
 
   const [currentImage, setCurrentImage] = useState(0);
-  const [textValue, setTextValue] = useState(""); // Estado para o valor do textarea
+  const [textValue, setTextValue] = useState("");
   const textAreaRef = useRef(null);
 
   const nextImage = () => {
-    setCurrentImage((currentImage + 1) % images.length);
+    setCurrentImage((currentImage + 1) % sections[0].images.length);
   };
 
   const prevImage = () => {
-    setCurrentImage((currentImage - 1 + images.length) % images.length);
+    setCurrentImage((currentImage - 1 + sections[0].images.length) % sections[0].images.length);
   };
 
-  // Função para ajustar automaticamente a altura do textarea
   const handleInputChange = (e) => {
     const textArea = textAreaRef.current;
     setTextValue(e.target.value);
-
-    // Redefine a altura para calcular o tamanho correto
     textArea.style.height = "100px";
     textArea.style.height = `${textArea.scrollHeight}px`;
   };
 
   return (
     <Container>
-      {/* Cabeçalho */}
-      <HeaderLogado>
-      </HeaderLogado>
+      <HeaderLogado />
 
-      {/* Conteúdo Principal */}
       <MainContent>
-        {/* Seção de Pedido de Oração */}
         <PrayerSection>
           <h2>Pedido de oração</h2>
-
-          {/* Label e Input para Pessoas para oração */}
           <label htmlFor="pessoasOracao">Pessoas para oração</label>
-          <input
-            type="text"
-            id="pessoasOracao"
-            maxLength ={50}
-            // placeholder="Pessoas para oração"
-          />
-
-          {/* Label e Textarea para Motivo de oração */}
+          <input type="text" id="pessoasOracao" maxLength={50} />
           <label htmlFor="motivoOracao">Motivo de oração</label>
           <textarea
             ref={textAreaRef}
             id="motivoOracao"
-            // placeholder="Motivo de oração"
             rows="1"
-            maxLength ={200}
+            maxLength={200}
             value={textValue}
             onChange={handleInputChange}
           />
-
           <button>Enviar</button>
         </PrayerSection>
 
-        {/* Carrossel de Agenda e Louvores */}
         <CarouselContainer>
-          {/* Agenda da Semana */}
           <Section>
             <h2>Agenda da Semana</h2>
             <Carousel>
-              <ArrowButton onClick={prevImage}>{"<"}</ArrowButton>
-              <ImageContainer>
-                <img src={images[currentImage]} alt="Agenda da Semana" />
-              </ImageContainer>
-              <ArrowButton onClick={nextImage}>{">"}</ArrowButton>
+            <button onClick={() => swiperRef.current.slidePrev()}>
+            <FaChevronLeft size={30} color="black" />
+          </button>
+            <Swiper
+              modules={[Pagination, A11y, Navigation, Scrollbar]}
+              slidesPerView={2}
+              spaceBetween={90}
+              onSwiper={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+              onSlideChange={(swiper) => setCurrentImage(swiper.activeIndex)}
+            >
+              {sections[0].images.map((item, index) => (
+                <SwiperSlide key={index}>
+                  <img src={item.url} alt={`Slider ${index + 1}`} className={styles.sliderItem} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <button onClick={() => swiperRef.current.slideNext()}>
+            <FaChevronRight size={30} color="black" />
+          </button>
             </Carousel>
           </Section>
 
-          {/* Indicações de Louvores */}
           <Section>
             <h2>Indicações de Louvores</h2>
             <Carousel>
-              <ArrowButton onClick={prevImage}>{"<"}</ArrowButton>
-              <ImageContainer>
-                <img src={images[currentImage]} alt="Indicações de Louvores" />
-              </ImageContainer>
-              <ArrowButton onClick={nextImage}>{">"}</ArrowButton>
+            <button onClick={() => swiperRef.current.slidePrev()}>
+            <FaChevronLeft size={30} color="black" />
+          </button>
+            <Swiper
+              modules={[Pagination, A11y, Navigation, Scrollbar]}
+              slidesPerView={2}
+              spaceBetween={90}
+              onSwiper={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+              onSlideChange={(swiper) => setCurrentImage(swiper.activeIndex)}
+            >
+              {sections[0].images.map((item, index) => (
+                <SwiperSlide key={index}>
+                  <img src={item.url} alt={`Slider ${index + 1}`} className={styles.sliderItem} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <button onClick={() => swiperRef.current.slideNext()}>
+            <FaChevronRight size={30} color="black" />
+          </button>
             </Carousel>
           </Section>
         </CarouselContainer>
