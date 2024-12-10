3136a6670ca7e406d24b94622b89e58966dbb146
gustavobfonseca <gustavo.fonseca@sptech.school>
2024-12-10 20:41:37 -0300

    refatoracao da tela doe

--- front-jk/src/pages/Doe/Doe.jsx
+++ front-jk/src/pages/Doe/Doe.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Doe.module.css';
 import HeaderLogado from '../../components/HeaderLogado/HeaderLogado';
 import Titulo from '../../components/Titulo/Titulo';
@@ -6,22 +6,82 @@ import FormContainer from '../../components/FormContainer/FormContainer';
 import Form from '../../components/Form/Form';
 import Input from '../../components/Input/Input';
 import Botao from '../../components/TelaCadastro/botao/Botao';
-import ImageContainer from'../../components/ImageContainer/ImageContainer'
-import lateral from '../../img/doe.png'
+import ImageContainer from '../../components/ImageContainer/ImageContainer';
 import Container from '../../components/Container/Container';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import lateral from '../../img/doe.png';
 
 const Doe = () => {
     const [verificado, setVerificado] = useState(false); // Inicialmente não verificado
     const [linhaSelecionada, setLinhaSelecionada] = useState(null);
+    const [formData, setFormData] = useState({
+        endereco: {
+            cep: '',
+            logradouro: '',
+            numero: '',
+            complemento: '',
+            bairro: '',
+            localidade: '',
+            uf: ''
+        }
+    });
 
     const familia = {
         nomeFamilia: "Família Silva",
         membros: [
-            { nome: "Robert Silva", dataNascimento: "20/02/1986", responsavel: false },
-            { nome: "Andressa Silva", dataNascimento: "20/02/1986", responsavel: false },
-            { nome: "Anderson Silva", dataNascimento: "20/02/1986", responsavel: false }
+            { nome: "Robert Silva", dataNascimento: "20/02/1986", telefone: "teste endereco", },
+            { nome: "Andressa Silva", dataNascimento: "20/02/1986", telefone: "11933736363" },
+            { nome: "Anderson Silva", dataNascimento: "20/02/1986", telefone: "11933736363" }
         ]
     };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name in formData.endereco) {
+            setFormData((prevState) => ({
+                ...prevState,
+                endereco: {
+                    ...prevState.endereco,
+                    [name]: value
+                }
+            }));
+        }
+    };
+
+    useEffect(() => {
+        const fetchAddress = async () => {
+            if (formData.endereco.cep.length === 8) {
+                try {
+                    const response = await axios.get(`https://viacep.com.br/ws/${formData.endereco.cep}/json/`);
+                    if (response && response.data) {
+                        const { logradouro, bairro, localidade, uf } = response.data;
+                        setFormData((prevState) => ({
+                            ...prevState,
+                            endereco: {
+                                ...prevState.endereco,
+                                logradouro,
+                                bairro,
+                                localidade,
+                                uf
+                            }
+                        }));
+                    } else {
+                        console.error('Response incorreta', response);
+                    }
+                } catch (error) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Não foi possível encontrar o endereço para o CEP informado.',
+                        confirmButtonText: 'OK',
+                    });
+                }
+            }
+        };
+        fetchAddress();
+    }, [formData.endereco.cep]);
 
     const handleVerificar = (event) => {
         event.preventDefault();
@@ -36,95 +96,64 @@ const Doe = () => {
         <>
             <HeaderLogado />
             <div className={styles.container}>
-            <ImageContainer image={lateral}>
-            </ImageContainer>
-            <FormContainer>
-                {verificado ? (<>
-                
-                <Titulo>Selecione o Responsável</Titulo>
-                    <div className={styles.cardFamilia}>
-                        <div className={styles.boxTitulo}>
-                            <h2>{familia.nomeFamilia}</h2>
-                        </div>
-                        <div className={styles.dados}>
-                            <div className={styles.campo}>
+                <ImageContainer image={lateral} />
+                <FormContainer>
+                    {verificado ? (
+                        <>
+                            <Titulo>Selecione o Responsável</Titulo>
+                            <div className={styles.cardFamilia}>
                                 <div className={styles.boxTitulo}>
-                                <h3>Nome</h3>
+                                    <h2>{familia.nomeFamilia}</h2>
                                 </div>
-                                <div className={styles.boxLinha}>
-                                {familia.membros.map((membro, index) => (
-                                    <p
-                                        key={index}
-                                        className={`${styles.linha} ${linhaSelecionada === index ? styles.linhaSelecionada : ''}`}
-                                        onClick={() => selecionarLinha(index)}
-                                    >
-                                        {membro.nome}
-                                    </p>
-                                ))}
-                            </div>
-                            </div>
-
-                            <div className={styles.campo}>
-                            <div className={styles.boxTitulo}>
-                                <h3>Dt Nasc.</h3>
+                                <div className={styles.dados}>
+                                    <table className={styles.tabela}>
+                                        <thead>
+                                            <tr>
+                                                <th>Nome</th>
+                                                <th>Dt Nasc.</th>
+                                                <th>Telefone</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {familia.membros.map((membro, index) => (
+                                                <tr
+                                                    key={index}
+                                                    className={`${styles.linha} ${linhaSelecionada === index ? styles.linhaSelecionada : ''}`}
+                                                    onClick={() => selecionarLinha(index)}
+                                                >
+                                                    <td>{membro.nome}</td>
+                                                    <td>{membro.dataNascimento}</td>
+                                                    <td>{membro.telefone}</td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
                                 </div>
-                                <div className={styles.boxLinha}>
-
-                                {familia.membros.map((membro, index) => (
-                                    <p
-                                        key={index}
-                                        className={`${styles.linha} ${linhaSelecionada === index ? styles.linhaSelecionada : ''}`}
-                                        onClick={() => selecionarLinha(index)}
-                                    >
-                                        {membro.dataNascimento}
-                                    </p>
-                                ))}
-                                </div>
-                            </div>
-
-                            <div className={styles.campo}>
-                            <div className={styles.boxTitulo}>
-                                <h3>Responsável</h3>
-                                </div>
-                                <div className={styles.boxCheck}>                                
-
-                                {familia.membros.map((membro, index) => (
-                                    <div
-                                        key={index}
->
-                                        <div className={styles}>
-
-                                        <input
-                                            type="radio"
-                                            name="responsavel"
-                                            checked={linhaSelecionada === index}
-                                            />
-                                            </div>
+                                <div style={{ display: "flex", justifyContent: "center" }}>
+                                    <div style={{ width: "98%" }}>
+                                        <Botao onClick={() => alert("Doação realizada!")}>
+                                            Doar
+                                        </Botao>
                                     </div>
-                                ))}
                                 </div>
                             </div>
-                        </div>
-                        <Botao onClick={() => alert("Doação realizada!")}>Doar</Botao>
-                    </div>
-                    </>
-                ) : (
-<>
-<Titulo>Retirar Doação</Titulo>
-<Form onSubmit={handleVerificar}>
-                        <Input label="CEP" type="text" name="cep" />
-                        <Input readOnly label="Rua" style={{ background: '#f0f0f0', border: 'none' }} type="text" name="logradouro" />
-                        <Input label="Número" type="text" name="numero" />
-                        <Input label="Complemento" type="text" name="complemento" />
-                        <Input readOnly label="Bairro" type="text" name="bairro" style={{ backgroundColor: '#f0f0f0', border: 'none' }} />
-                        <Input readOnly label="Cidade" type="text" name="localidade" style={{ backgroundColor: '#f0f0f0', border: 'none' }} />
-                        <Input readOnly label="UF" type="text" name="uf" style={{ backgroundColor: '#f0f0f0', border: 'none' }} />
-                        <Botao type="submit">Verificar</Botao>
-                    </Form>
-</>
-                )}
-            </FormContainer>
-            
+                        </>
+                    ) : (
+                        <>
+                            <Titulo>Retirar Doação</Titulo>
+                            <Form onSubmit={handleVerificar}>
+                                <Input label="CEP" type="text" name="cep" value={formData.endereco.cep} onChange={handleChange} />
+                                <Input readOnly label="Rua" type="text" name="logradouro" value={formData.endereco.logradouro} />
+                                <Input label="Número" type="text" name="numero" value={formData.endereco.numero} onChange={handleChange} />
+                                <Input label="Complemento" type="text" name="complemento" value={formData.endereco.complemento} onChange={handleChange} />
+                                <Input readOnly label="Bairro" type="text" name="bairro" value={formData.endereco.bairro} />
+                                <Input readOnly label="Cidade" type="text" name="localidade" value={formData.endereco.localidade} />
+                                <Input readOnly label="UF" type="text" name="uf" value={formData.endereco.uf} />
+                                <Botao type="submit">Verificar</Botao>
+                            </Form>
+                        </>
+                    )}
+                </FormContainer>
             </div>
         </>
     );
